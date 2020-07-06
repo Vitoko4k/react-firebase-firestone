2b03e2aed2c3993f17e391b10fbb86af9b729c0d
Vitoko4k <vicsan2013@gmail.com>
2020-07-06 16:53:21 -0400

    LinksForm con diseño

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Links from './componets/Links'
-import LinksForm from './componets/Links'
+import LinksForm from './componets/LinksForm'
 
 function App() {
   return (
-    <div>
-    <Links />
-    <LinksForm />
+    <div className='container p-4'>
+      <div className='row'>
+      <LinksForm />
+      <Links />
+      </div>
     </div>
   );
 }
--- src/componets/LinksForm.js
+++ src/componets/LinksForm.js
@@ -1,10 +1,33 @@
 import React from 'react'
-import Links from './Links';
 
 export const LinksForm = () => {
     return (
-        <form>
-            
+        <form className='card card-body'>
+            <div className='form-group input-group'>
+                <div className='input-group-text bg-light'>
+                    <i className='material-icons'>insert_link</i>
+                </div>
+                <input 
+                type='text' 
+                className='form-control'
+                placeholder='https://algunurl.com'
+                name='url'/>
+            </div>
+            <div className='form-group input-group'>
+                <div className='input-group-text bg-light'>
+                    <i className='material-icons'>create</i>
+                </div>
+                <input
+                type='text'
+                className='form-control'
+                name='name'
+                placeholder='Nombre del sitio web'
+                />
+            </div>
+            <div className='form-group'>
+                <textarea name='descripcion' rpws='3' className='form-control'
+                placeholder='Escribe una descripcion'></textarea>
+            </div>
         </form>
     )
 };
